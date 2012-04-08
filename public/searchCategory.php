f4e93cccde873b9dc844cb8298bc17a3aeddeddb
Andrew Rivett <veggiefrog@gmail.com>
2012-04-08 19:13:15 -0400

    Added ability to live search category when adding faq

---
+++ public/searchCategory.php
@@ -0,0 +1,71 @@
+<?php
+//
+// Description
+// -----------
+// Search faqs by subject and date
+//
+// Arguments
+// ---------
+// user_id: 		The user making the request
+// search_str:		The search string provided by the user.
+// 
+// Returns
+// -------
+//
+function ciniki_atdo_searchCategory($ciniki) {
+    //  
+    // Find all the required and optional arguments
+    //  
+    require_once($ciniki['config']['core']['modules_dir'] . '/core/private/prepareArgs.php');
+    $rc = ciniki_core_prepareArgs($ciniki, 'no', array(
+        'business_id'=>array('required'=>'yes', 'blank'=>'no', 'errmsg'=>'No business specified'), 
+        'start_needle'=>array('required'=>'yes', 'blank'=>'yes', 'errmsg'=>'No search specified'), 
+        'limit'=>array('required'=>'yes', 'blank'=>'no', 'errmsg'=>'No limit specified'), 
+        )); 
+    if( $rc['stat'] != 'ok' ) { 
+        return $rc;
+    }   
+    $args = $rc['args'];
+    
+    //  
+    // Make sure this module is activated, and
+    // check permission to run this function for this business
+    //  
+    require_once($ciniki['config']['core']['modules_dir'] . '/atdo/private/checkAccess.php');
+    $rc = ciniki_atdo_checkAccess($ciniki, $args['business_id'], 'ciniki.atdo.searchCategory', 0); 
+    if( $rc['stat'] != 'ok' ) { 
+        return $rc;
+    }   
+
+	//
+	// Get the number of faqs in each status for the business, 
+	// if no rows found, then return empty array
+	//
+	$strsql = "SELECT category AS name "
+		. "FROM ciniki_atdos "
+		. "WHERE ciniki_atdos.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+		. "AND ciniki_atdos.status  "
+		. "AND (category LIKE '" . ciniki_core_dbQuote($ciniki, $args['start_needle']) . "%' "
+			. "AND category <> '' "
+			. ") "
+		. "";
+	$strsql .= "ORDER BY category "
+		. "";
+	if( isset($args['limit']) && $args['limit'] != '' && $args['limit'] > 0 ) {
+		$strsql .= "LIMIT " . ciniki_core_dbQuote($ciniki, $args['limit']) . " ";
+	} else {
+		$strsql .= "LIMIT 25 ";
+	}
+	ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbHashQueryTree');
+	$rc = ciniki_core_dbHashQueryTree($ciniki, $strsql, 'atdo', array(
+		array('container'=>'categories', 'fname'=>'name', 'name'=>'category', 'fields'=>array('name')),
+		));
+	if( $rc['stat'] != 'ok' ) {
+		return $rc;
+	}
+	if( !isset($rc['categories']) ) {
+		return array('stat'=>'ok', 'categories'=>array());
+	}
+	return array('stat'=>'ok', 'categories'=>$rc['categories']);
+}
+?>
